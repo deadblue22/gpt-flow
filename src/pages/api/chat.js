7ab8d6b4c40efcbe82da636cc05b4dc9281684e0
Deadblue <himmelex@gmail.com>
2023-05-13 02:51:13 +0800

    Add eslint

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,12 +1,13 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 const completion = await openai.createCompletion({
-model: "text-davinci-003",
-prompt: "Hello world",
+  model: "text-davinci-003",
+  prompt: "Hello world",
 });
+
 console.log(completion.data.choices[0].text);
