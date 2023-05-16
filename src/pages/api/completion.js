ef9483b72dccbe44534615e32c374922ea192f99
Zhao Zhenda <zhaozhenda@chuangxin.com>
2023-05-16 15:55:58 +0800

    Add chat & completion API

--- src/pages/api/completion.js
+++ src/pages/api/completion.js
@@ -1,7 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const apiKey = process.env.OPENAI_API_KEY;
-const modelName = "gpt-35-turbo-0301";
+const modelName = "text-davinci-003";
 const apiVersion = "2023-03-15-preview";
 const basePath = process.env.OPENAI_BASE_PATH + modelName;
 const configuration = new Configuration({
@@ -17,8 +17,8 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
-  const completion = await openai.createChatCompletion({
-    messages: req.message,
+  const completion = await openai.createCompletion({
+    prompt: req.prompt,
   });
-  res.status(200).json({ text: completion.data.choices[0].message });
+  res.status(200).json({ text: completion.data.choices[0].text });
 }
